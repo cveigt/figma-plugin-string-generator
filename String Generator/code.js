5d0c0cb4b12df031288ff10a0b1e4932d6e56241
cveigt <cveigt@gmail.com>
2021-10-01 22:39:01 -0700

    feat(added value generation)

--- String Generator/code.js
+++ String Generator/code.js
@@ -1,1 +1,55 @@
-throw new Error("This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`.")
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+figma.showUI(__html__);
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+
+figma.ui.onmessage = (msg) => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === "create-fake-date") {
+    const nodes = [];
+    const text = figma.createText();
+    text.characters = new Date().toDateString();
+    nodes.push(text);
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
+  if (msg.type === "create-fake-api-key") {
+    const nodes = [];
+    const text = figma.createText();
+    text.characters = generateApiKey();
+    nodes.push(text);
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
+  if (msg.type === "create-fake-datetime") {
+    const nodes = [];
+    const text = figma.createText();
+    text.characters = new Date().toString();
+    nodes.push(text);
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};
+
+function generateApiKey() {
+  const length = 40;
+  var result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
